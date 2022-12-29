f408ac0ba5d92575cee436649c8d5f182b2d652e
aaschuster <aaronjschuster@gmail.com>
2022-12-29 18:02:28 -0500

    style header and subheader

--- src/header.js
+++ src/header.js
@@ -1,11 +1,15 @@
 import React from "react";
 import styled from 'styled-components';
 
+const StyledH1 = styled.h1`
+    font-family: Optima, sans-serif;   
+`;  
+
 const Header = () => {
     return (
-        <h1>
+        <StyledH1>
             Isn't the universe an amazing place..?
-        </h1>
+        </StyledH1>
     )
 }
 
--- src/subheader.js
+++ src/subheader.js
@@ -1,11 +1,22 @@
 import React from "react";
+import styled from 'styled-components';
+
+const StyledSubHeader = styled.div`
+    font-family: Optima, sans-serif;
+    color: dimgray;   
+`;  
+
+const StyledDate =  styled.p`
+    font-style: italic;
+    font-size: 0.8rem;
+`
 
 const SubHeader = ( {title, date} ) => {
     return (
-        <div id="subheader">
+        <StyledSubHeader>
             <h2>{title}</h2>
-            <p>{date}</p>
-        </div>
+            <StyledDate>{date}</StyledDate>
+        </StyledSubHeader>
     )
 }
 
