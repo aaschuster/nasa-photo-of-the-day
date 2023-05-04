d050adb0407d1ad2e0865d5ffac66720ffc63b5f
Aaron Schuster <aaronjschuster@gmail.com>
2023-05-04 17:32:38 -0400

    updated dummy data

--- src/dummydata.js
+++ src/dummydata.js
@@ -1,8 +1,8 @@
 const dummyData = {
-    title: "The greatest roller coaster ever built",
-    date: "8/16/2017",
-    copyright: "Jon Stinchcomb/News Herald",
-    hdurl: "https://www.gannett-cdn.com/-mm-/1893984dda7012d6ecf90758428c1acee2860fc2/c=0-305-6000-3695/local/-/media/2017/08/16/OHGroup/PortClinton/636384950635231886-1-Cedar-Point-IMG-0498.JPG?width=1320&height=746&fit=crop&format=pjpg&auto=webp",
+    title: "Waiting for liftoff... (awaiting API response)",
+    date: "11/3/2010",
+    copyright: "NASA/Kim Shiflett",
+    hdurl: "https://www.nasa.gov/images/content/495853main_10-11-03-1-1600_946-710.jpg",
 }
 
 export default dummyData;
